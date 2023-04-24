00da58536f7b3d7e247ace3012083dcd91d5a4ce
LuigiBelanda <luigi.belanda.milani@gmail.com>
2023-04-24 16:39:46 -0300

    Char in Java

---
+++ variables/varChar.java
@@ -0,0 +1,16 @@
+package variables;
+
+public class varChar {
+    public static void main(String[] args) {
+        char potions = 'A';
+        char arithmancy = 'B';
+        char charms = 'C';
+
+        System.out.println("Potions: " + potions);
+        System.out.println("Arithmancy: " + arithmancy);
+        System.out.println("Charms: " + charms);
+
+        // char = 1 caracter só
+        // Utiliza aspas simples
+    }
+}
