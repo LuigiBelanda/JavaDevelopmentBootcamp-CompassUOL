5538df2f1857bd6002e058b0a23b07234b40df7c
LuigiBelanda <luigi.belanda.milani@gmail.com>
2023-04-24 17:05:51 -0300

    Double in Java

---
+++ variables/varDouble.java
@@ -0,0 +1,12 @@
+package variables;
+
+public class varDouble {
+    public static void main(String[] args) {
+        // double = usado para guardar casas decimais
+        // É recomendado usar ele sempre que for realizar uma conta matemática
+
+        double money = 20;
+        int peoples = 3;
+        System.out.println(money / peoples);
+    }
+}
