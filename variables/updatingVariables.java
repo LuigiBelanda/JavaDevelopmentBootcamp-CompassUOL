0d6958085fb6e6b0d46d039a798ee6c6c5df2bfe
LuigiBelanda <luigi.belanda.milani@gmail.com>
2023-04-24 15:23:41 -0300

    Variables in Java

---
+++ variables/updatingVariables.java
@@ -0,0 +1,20 @@
+package variables;
+
+public class updatingVariables {
+    public static void main(String[] args) {
+        int peoples = 5;
+        System.out.println(peoples);
+
+        peoples = peoples + 1;
+        System.out.println(peoples);
+
+        peoples += 1;
+        System.out.println(peoples);
+
+        peoples = peoples - 1;
+        System.out.println(peoples);
+
+        peoples -= 1;
+        System.out.println(peoples);
+    }
+}
