f29c04e64e0c1c2d80e4a39703d13fa2c86b2208
LuigiBelanda <luigi.belanda.milani@gmail.com>
2023-04-24 14:16:03 -0300

    Firts commit - First code in Java

---
+++ getting_started/HelloJava.java
@@ -0,0 +1,9 @@
+package getting_started;
+
+// Classe relacionada ao nome do arquivo
+public class HelloJava {
+    // Classe principal do programa
+    public static void main(String[] args) {
+        System.out.println("Hello Java");
+    }
+}
