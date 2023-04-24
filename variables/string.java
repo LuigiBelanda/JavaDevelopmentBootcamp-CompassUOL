6ffe4f66a5a8fe5a9164c8c598b5bef61a659797
LuigiBelanda <luigi.belanda.milani@gmail.com>
2023-04-24 15:29:11 -0300

    String in Java

---
+++ variables/string.java
@@ -0,0 +1,13 @@
+package variables;
+
+public class string {
+    public static void main(String[] args) {
+        String name = "Luigi";
+        System.out.println(name);
+
+        int age = 19;
+        System.out.println(age);
+
+        System.out.println("My name is " + name + "and my age is " + age);
+    }
+}
