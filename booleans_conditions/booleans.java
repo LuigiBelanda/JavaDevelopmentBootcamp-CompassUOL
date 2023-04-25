47ea45935a4e3605f5a1eabc9d5755a7b02ff939
LuigiBelanda <luigi.belanda.milani@gmail.com>
2023-04-25 09:20:17 -0300

    Booleans in Java

---
+++ booleans_conditions/booleans.java
@@ -0,0 +1,14 @@
+package booleans_conditions;
+
+public class booleans {
+    public static void main(String[] args) {
+        // Guarda apenas 1 bit na memória e só são possíveis
+        // 2 valores, true ou false 
+        
+        boolean bool1 = true;
+        boolean bool2 = false;
+
+        System.out.println(bool1);
+        System.out.println(bool2);
+    }
+}
