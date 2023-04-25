3701feddbe8b6073720fb59a47bc939c0807545e
LuigiBelanda <luigi.belanda.milani@gmail.com>
2023-04-25 14:41:35 -0300

    Functions in Java

---
+++ func_java/Chorus.java
@@ -0,0 +1,19 @@
+package func_java;
+
+public class Chorus {
+    public static void main(String[] args) {
+        // Chorus needs to be sung 5 times
+        signChorus();
+        signChorus();
+        signChorus();
+        signChorus();
+        signChorus();
+    }
+
+    public static void signChorus() {
+        System.out.println("Don't blame it on the sunshine");
+        System.out.println("Don't blame it on the moonlight");
+        System.out.println("Don't blame it on good times");
+        System.out.println("Don't blame it on the boogie\n");
+    }
+}
