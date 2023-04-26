167e1dc8fdb9ec4caa552024a8399257cdfa6048
LuigiBelanda <luigi.belanda.milani@gmail.com>
2023-04-26 11:00:19 -0300

    Arrays in Java

---
+++ arrays/IntroToArrays.java
@@ -0,0 +1,17 @@
+package arrays;
+
+public class IntroToArrays {
+    public static void main(String[] args) {
+        String[] kingdoms = { "Marcia", "Wessex", "Northumbria", "East Anglia" };
+
+        System.out.println(kingdoms[0]);
+        System.out.println(kingdoms[1]);
+        System.out.println(kingdoms[2]);
+        System.out.println(kingdoms[3]);
+
+        // String kingdom1 = "Mercia";
+        // String kingdom2 = "Wessex";
+        // String kingdom3 = "Northumbria";
+        // String kingdom4 = "East Anglia";
+    }
+}
