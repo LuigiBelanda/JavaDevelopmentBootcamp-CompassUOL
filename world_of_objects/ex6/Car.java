c2dbbe128f3d3cc6a6535c7fb61ede0cd895bf14
LuigiBelanda <luigi.belanda.milani@gmail.com>
2023-04-26 16:40:17 -0300

    Methods Objects in Java

---
+++ world_of_objects/ex6/Car.java
@@ -0,0 +1,74 @@
+package world_of_objects.ex6;
+
+public class Car {
+    // Atributos
+    private String make;
+    private double price;
+    private int year;
+    private String color;
+
+
+
+    // Construtor
+    public Car (String make, double price, int year, String color) {
+        this.make = make;
+        this.price = price;
+        this.year = year;
+        this.color = color;
+    }
+
+
+
+    // Constructor copy
+    public Car (Car source) {
+        this.make = source.make;
+        this.price = source.price;
+        this.year = source.year;
+        this.color = source.color;
+    }
+
+    
+
+    // Getters
+    public String getMake() {
+        return make;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+
+
+    // Setters
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+
+
+    // Métodos
+    public void drive() {
+        System.out.print("\nDrive to the exit - " + make + " - " + year + " - " + color + " \n");
+    }
+}
---
+++ world_of_objects/ex6/Main.java
@@ -0,0 +1,61 @@
+package world_of_objects.ex6;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        // Sem construtor 
+
+        /*
+        Car nissan = new Car();
+
+        nissan.make = "Nissan";
+        nissan.price = 10000;
+        nissan.year = 2002;
+        nissan.color = "Green";
+        */
+
+        // Com construtor
+        Car nissan = new Car("Nissa", 10000, 2002, "Green");
+        System.out.println("Make: " + nissan.getMake() + "\tPrice: " + nissan.getPrice() + "\tYear: " + nissan.getYear() + "\tColor: " + nissan.getColor());
+    
+        nissan.setColor("Jet Black");
+        nissan.setPrice(nissan.getPrice() / 2);
+        System.out.println("Make: " + nissan.getMake() + "\tPrice: " + nissan.getPrice() + "\tYear: " + nissan.getYear() + "\tColor: " + nissan.getColor());
+    
+
+        // Aqui passamos como referência para o objeto nissan2 o objeto nissan
+        // Se fizermos alguma mudança em alguns dos dois, essa mudança reflete no original e vice-versa
+        Car nissan2 = nissan;
+
+        nissan2.setColor("Yellow");
+        nissan.setColor("Orange");
+        nissan2.setColor("Blue");
+        nissan.setColor("Purple");
+        nissan2.setColor("Fuchsia");
+        nissan.setColor("Beige");
+        System.out.println("\n\nMake: " + nissan.getMake() + "\tPrice: " + nissan.getPrice() + "\tYear: " + nissan.getYear() + "\tColor: " + nissan.getColor());
+        System.out.println("Make: " + nissan2.getMake() + "\tPrice: " + nissan2.getPrice() + "\tYear: " + nissan2.getYear() + "\tColor: " + nissan2.getColor());
+    
+    
+        // Copy constructor, estamos criando um nosso objeto nissan3 passando como parametro o 
+        // objeto nissan, assim se quisermos mudar algo no nissan3 podemos mudar
+        // pois ele não referência o objeto nissan original
+        Car nissan3 = new Car(nissan);
+        
+        nissan3.setColor("Yellow");
+        nissan.setColor("Orange");
+        nissan3.setColor("Blue");
+        nissan.setColor("Purple");
+        nissan3.setColor("Fuchsia");
+        nissan.setColor("Beige");
+        System.out.println("\n\nMake: " + nissan.getMake() + "\tPrice: " + nissan.getPrice() + "\tYear: " + nissan.getYear() + "\tColor: " + nissan.getColor());
+        System.out.println("Make: " + nissan3.getMake() + "\tPrice: " + nissan3.getPrice() + "\tYear: " + nissan3.getYear() + "\tColor: " + nissan3.getColor());
+    
+    
+    
+        // Usando método
+        nissan3.drive();
+    }
+}
+
+        
