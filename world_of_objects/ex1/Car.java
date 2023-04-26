8c766133d8d7f0da517854e24191d5d4f1d823df
LuigiBelanda <luigi.belanda.milani@gmail.com>
2023-04-26 14:10:46 -0300

    Objects in Java

---
+++ world_of_objects/ex1/Car.java
@@ -0,0 +1,8 @@
+package world_of_objects.ex1;
+
+public class Car {
+    String make;
+    double price;
+    int year;
+    String color;
+}
---
+++ world_of_objects/ex1/Main.java
@@ -0,0 +1,11 @@
+package world_of_objects.ex1;
+
+public class Main {
+    public static void main(String[] args) {
+        Car nissan = new Car();
+        nissan.make = "Nissan";
+        nissan.price = 10000;
+        nissan.year = 2002;
+        nissan.color = "Green";
+    }
+}
