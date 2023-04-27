bb0daa7dcd63dabbe00f41e5627940bb4fb929d6
LuigiBelanda <luigi.belanda.milani@gmail.com>
2023-04-27 12:58:55 -0300

    Dealership Project

---
+++ world_of_objects/ex10/Dealership.java
@@ -0,0 +1,17 @@
+package world_of_objects.ex10;
+
+import java.util.Arrays;
+
+public class Dealership {
+    private Car[] cars;
+
+    public Dealership(Car[] cars) {
+        this.cars = new Car[cars.length];
+
+        for (int i = 0; i < this.cars.length; i++) {
+            this.cars[i] = new Car(cars[i]);
+        }
+        
+        // this.cars = Arrays.copyOf(cars, cars.length);
+    }
+}
