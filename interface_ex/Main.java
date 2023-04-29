1e99c4571d5130a0c403a65e7959ad9454d49a44
LuigiBelanda <luigi.belanda.milani@gmail.com>
2023-04-29 14:08:03 -0300

    CRUD in Repository

--- interface_ex/Main.java
+++ interface_ex/Main.java
@@ -16,19 +16,28 @@ public class Main {
         // Iniciando o repositório
         AccountRepository repository = new AccountRepository();
 
-
         // Assume these were obtained from user input.
         List<Account> accounts = Arrays.asList(
                 new Checking("A1234B", new BigDecimal("500.00")),
                 new Checking("E3456F", new BigDecimal("300.50")),
                 new Checking("I5678J", new BigDecimal("100.25")),
                 new Credit("A1534B", new BigDecimal("0.50")),
-                new Credit("G4567H", new BigDecimal("200.00"))
-        );
+                new Credit("G4567H", new BigDecimal("200.00")));
 
+        // Salvando as contas
         accounts.forEach(account -> {
             repository.createAccount(account);
         });
+
+        // Recuperando as contas
+        Checking checking = (Checking) repository.retrieveAccount("A1234B");
+
+        // Mudando dados
+        checking.setBalance(checking.getBalance().add(new BigDecimal("100")));
+        repository.updateAccount(checking);
+
+        // Deletando conta
+        repository.deleteAccount("G4567H");
 
     }
 
