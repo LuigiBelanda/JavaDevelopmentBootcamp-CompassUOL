3836df4abc19ae60ec27ba6fe34039ef65b02db2
LuigiBelanda <luigi.belanda.milani@gmail.com>
2023-04-30 11:22:44 -0300

    Creating a thread

---
+++ concurrency_multithreading/start_concept/MultiThread.java
@@ -0,0 +1,34 @@
+package concurrency_multithreading.start_concept;
+
+public class MultiThread {
+    public static void main(String[] args) {
+
+        // Criando uma Thread
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                longTask();
+            };
+        };
+        Thread thread2 = new Thread(runnable);
+
+        // Executando uma thread
+        thread2.run();
+
+        System.out.println("Doing other work on main thread");
+
+    }
+
+    public static void longTask() {
+        long t = System.currentTimeMillis();
+        long end = t + 3000;
+
+        while (true) {
+            if (System.currentTimeMillis() == end) {
+                System.out.println("Finished long task");
+                break;
+            }
+        }
+    }
+
+}
